9546beb70d3c6f127a4c6ee6aee85b2d8e30bf02
Paula C. Sanematsu <paulasan@gmail.com>
2020-10-08 10:33:00 -0400

    Remove povray movie output

--- exampleHeterogeneousCellTypes.cpp
+++ exampleHeterogeneousCellTypes.cpp
@@ -1,7 +1,6 @@
 #include "core/PeriodicBox.h"
 #include "core/Tessellation.h"
 #include "core/CellType.h"
-#include "misc/other/MovieCreator.h"
 
 #include "core/Cell-inline.h"
 
@@ -61,8 +60,6 @@ int main(int argc, char** argv) {
 //    t.addCell(type2, position);
 //  }
   
-  MovieCreator mc;
-  mc.start();
   for(int i=0; i<=SimulationsSteps; ++i) {
     // updates of the Voronoi tesselation
     t.computeTopologyAndGeometry();
@@ -97,73 +94,10 @@ int main(int argc, char** argv) {
       totalSurface *= 0.5;  // counted twice:  sum of all cell surfaces = 2* total cell-cell interface
       std::cout << "12 interfaces: " << surface12 << " / " << totalSurface << " (" << surface12/totalSurface << ")" << std::endl;
      
-      
-      // this is to create the movie
-      t.saveAsImage(mc.nextFrameFileName(), [&t, &type1, Side](PovrayGenerator &g) {
-        g.addLight(Vector3D(10*Side,0,0), Color(0.75,0.70,0.70)); 
-        g.addLight(Vector3D(0,-10*Side,0), Color(0.30,0.30,0.30)); 
-        g.addLight(Vector3D(0,0,10*Side), Color(0.95,0.90,0.90)); 
-
-        const Vector3D BoxPosition1(-2*Side, -2*Side, -2*Side), BoxPosition2(2*Side, 2*Side, 0.5*Side);
-        PovrayMesh<int> meshForCutting;
-        meshForCutting.addPoint(0, Vector3D(0, 0, 0));
-        meshForCutting.addPoint(1, Vector3D(Side, 0, 0));
-        meshForCutting.addPoint(2, Vector3D(0, Side, 0));
-        meshForCutting.addPoint(3, Vector3D(0, 0, Side));
-        meshForCutting.addTriangle(0, 1, 2);
-        meshForCutting.addTriangle(0, 1, 3);
-        meshForCutting.addTriangle(0, 2, 3);
-        meshForCutting.addTriangle(1, 2, 3);
-        
-        for(const Cell *c : t.cells()) {
-//          if(c->type()==&type1) {
-//            g.startUnion();
-//            g.drawMesh(c->createMesh());
-//            g.endUnionWithColor((c->type()==&type1)?Color(0,0,1):Color(1,0,0));
-
-//            g.startUnionWithBoxIntersection(BoxPosition1, BoxPosition2);
-            g.startUnionWithMeshIntersection(meshForCutting);
-            g.drawMesh(c->createMesh());
-            g.endUnionWithColor((c->type()==&type1)?Color(0,0,1):Color(1,0,0));
-          //          }
-        }
-        
-//        g.startUnion();
-////        g.startUnionWithBoxIntersection(BoxPosition1, BoxPosition2);
-//        for(const Cell *c : t.cells()) {
-////          if(c->type()==&type1) {
-//            for(const DirectedFace *f : c->faces()) {
-//              DirectedEdgeOfCell *edge=f->firstEdge();
-//              do {
-//                VertexOfCell *v1 = edge->vertex();
-//                VertexOfCell *v2 = edge->nextAroundFace()->vertex();
-//                g.drawCylinder(v1->positionWithRespectTo(c->position()), v2->positionWithRespectTo(c->position()), 0.05);
-//                edge = edge->nextAroundFace();
-//              } while(edge!=f->firstEdge());
-//            }
-////          }
-//        }
-//        g.endUnionWithColor(Color(0.5, 0.5, 0.5));
-////        t.drawVerticesDefault(g, Color(0.5, 0.5, 0.5));
-//        g.startUnion();
-////        g.startUnionWithBoxIntersection(BoxPosition1, BoxPosition2);
-//        for(const Cell *c : t.cells()) {
-//          for(const VertexOfCell *v : c->vertices()) g.drawSphere(v->positionWithRespectTo(c->position()), 0.04);
-//        }
-//        g.endUnionWithColor(Color(0.5, 0.5, 0.5));
-//  
-//        for(const Cell *c : t.cells()) {
-//          g.startUnion();
-////          g.startUnionWithBoxIntersection(BoxPosition1, BoxPosition2);
-//          g.drawSphere(c->position(), 0.4);
-//          g.endUnionWithColor((c->type()==&type1)?Color(0,0,1):Color(1,0,0));
-//        }
-      }, 800, 800, Vector3D(1.6, -0.8, 1.37));
     }
     // compute time step
     t.timeStep(DeltaT);
   }
-  mc.createMovie("movie-cut.mp4", 20);
 
   return 0;
 }
